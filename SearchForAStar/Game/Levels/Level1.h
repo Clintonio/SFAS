600daad146062be68455bfd4a19d6c90b5e04aec
Clinton Alexander <admin@clintonio.com>
2013-06-09 03:07:22 +0100

    Added a new enemy and weapon

--- SearchForAStar/Game/Levels/Level1.h
+++ SearchForAStar/Game/Levels/Level1.h
@@ -6,7 +6,12 @@ using SFAS::Game::Level;
 #include "WeaponType.h"
 
 Level getLevel1() {
-	WeaponType* weaponTypes = new WeaponType[3];
+	Level::Enemy* level1Enemies = static_cast<Level::Enemy*> 
+			(::operator new (sizeof(Level::Enemy[1])));
+	Level::EnemyType* enemyTypes = new Level::EnemyType[4];
+	WeaponType* weaponTypes = new WeaponType[4];
+
+	// Define weapon types
 	weaponTypes[0].name = "None";
 	weaponTypes[0].ranged   = false;
 
@@ -32,7 +37,18 @@ Level getLevel1() {
 	weaponTypes[2].textureFile	= L"textures/rocket.png";
 	weaponTypes[2].weaponAIType = "target";
 
-	Level::EnemyType* enemyTypes = new Level::EnemyType[3];
+	weaponTypes[3].name			= "Fast Homing Rocket";
+	weaponTypes[3].damage		= 5;
+	weaponTypes[3].ranged		= true;
+	weaponTypes[3].fireDelay	= 3.0f;
+	weaponTypes[3].lifetime		= 15.0f;
+	weaponTypes[3].dimensions	= D3DXVECTOR3( 17.0f, 9.0f, 0.0f );
+	weaponTypes[3].soundFile	= "Sound/targettedrocket.wav";
+	weaponTypes[3].speed		= 200.0f;
+	weaponTypes[3].textureFile	= L"textures/rocket.png";
+	weaponTypes[3].weaponAIType = "target";
+
+	// Define enemy types
 	enemyTypes[0].id			= 0;
 	enemyTypes[0].health		= 1;
 	enemyTypes[0].aiType		= "suicide";
@@ -40,41 +56,29 @@ Level getLevel1() {
 	enemyTypes[0].dimensions	= D3DXVECTOR3(20.0f, 20.0f, 0.0f);
 	enemyTypes[0].textureFile	= L"textures/enemy.png";
 
-	enemyTypes[1].id			= 0;
+	enemyTypes[1].id			= 1;
 	enemyTypes[1].health		= 3;
 	enemyTypes[1].aiType		= "avoid";
 	enemyTypes[1].weaponType	= &weaponTypes[1];
-	enemyTypes[1].dimensions	= D3DXVECTOR3(25.0f, 25.0f, 0.0f);
+	enemyTypes[1].dimensions	= D3DXVECTOR3(20.0f, 25.0f, 0.0f);
 	enemyTypes[1].textureFile	= L"textures/enemy2.png";
 
-	enemyTypes[2].id			= 0;
+	enemyTypes[2].id			= 2;
 	enemyTypes[2].health		= 10;
 	enemyTypes[2].aiType		= "avoid";
 	enemyTypes[2].weaponType	= &weaponTypes[2];
-	enemyTypes[2].dimensions	= D3DXVECTOR3(30.0f, 30.0f, 0.0f);
+	enemyTypes[2].dimensions	= D3DXVECTOR3(20.0f, 30.0f, 0.0f);
 	enemyTypes[2].textureFile	= L"textures/enemy3.png";
-	
 
-	Level::Enemy* level1Enemies = static_cast<Level::Enemy*> 
-			(::operator new (sizeof(Level::Enemy[1])));
-	level1Enemies[0] = Level::Enemy (
-		2, D3DXVECTOR3(100,100,0)
-	);/*
-	level1Enemies[1] = Level::Enemy (
-		2, D3DXVECTOR3(200,500,0)
-	);
-	level1Enemies[2] = Level::Enemy (
-		2, D3DXVECTOR3(600,300,0)
-	);
-	level1Enemies[3] = Level::Enemy (
-		2, D3DXVECTOR3(300,500,0)
-	);
-	level1Enemies[4] = Level::Enemy (
-		2, D3DXVECTOR3(485,500,0)
-	);
-	level1Enemies[5] = Level::Enemy (
-		2, D3DXVECTOR3(600,235,0)
-	);*/
+	enemyTypes[3].id			= 3;
+	enemyTypes[3].health		= 15;
+	enemyTypes[3].aiType		= "avoid";
+	enemyTypes[3].weaponType	= &weaponTypes[3];
+	enemyTypes[3].dimensions	= D3DXVECTOR3(30.0f, 70.0f, 0.0f);
+	enemyTypes[3].textureFile	= L"textures/enemy4.png";
+	
+	// Define enemies
+	level1Enemies[0] = Level::Enemy (3, D3DXVECTOR3(100,100,0));
 
 	Level level1;
 	
@@ -83,7 +87,7 @@ Level getLevel1() {
 	level1.enemyCount		 = 1;
 	level1.enemies			 = level1Enemies;
 	level1.skyboxTextureFile = L"textures/skybox.png";
-	level1.enemyTypeCount	 = 3;
+	level1.enemyTypeCount	 = 4;
 	level1.enemyTypes		 = enemyTypes;
 	level1.weaponTypes		 = weaponTypes;
 
